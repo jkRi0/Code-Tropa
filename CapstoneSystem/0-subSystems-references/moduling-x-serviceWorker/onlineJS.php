ae7fb1630fc4bd4488addbdfc583478eea6e6942
jkRi0 <rioverosjustine92@gmail.com>
2025-09-27 11:32:56 +0800

    trying vercel and infinity integration

--- CapstoneSystem/0-subSystems-references/moduling-x-serviceWorker/onlineJS.php
+++ CapstoneSystem/0-subSystems-references/moduling-x-serviceWorker/onlineJS.php
@@ -1,7 +1,6 @@
 <?php
 
 header("Content-Type: text/plain"); // Set content type for plain text
-header("Access-Control-Allow-Origin: https://code-tropa.vercel.app"); // Allow requests from your Vercel frontend
 
 if (isset($_GET['message'])) {
     echo 'PHP endpoint received: ' . htmlspecialchars($_GET['message']);
