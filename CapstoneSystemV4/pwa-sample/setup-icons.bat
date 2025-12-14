203e17bd5cb2b24a3d763d78f3f11cdb2ff71f91
jkRi0 <rioverosjustine92@gmail.com>
2025-12-14 11:18:12 +0800

    a

--- CapstoneSystemV4/pwa-sample/setup-icons.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Opening icon generator in your default browser...
-echo Please click the buttons to generate and download icon-192.png and icon-512.png
-echo Make sure to save them in this directory.
-start generate-icons.html
-pause
-
